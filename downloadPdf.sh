0466557199e638ba88e3aa0231a5993318cd76bd
Tomohito Nakayama <tmht@7k8m.me>
2025-09-27 23:36:28 +0900

    Update PDFs

--- downloadPdf.sh
+++ downloadPdf.sh
@@ -1,3 +1,6 @@
 wget --recursive --accept .pdf https://www.entomophagy.or.jp/newsletter
 mv www.entomophagy.or.jp/_files/ugd/*.pdf ./
 rm -rf www.entomophagy.or.jp
+git add .
+git commit -m "Update PDFs"
+git push origin main
