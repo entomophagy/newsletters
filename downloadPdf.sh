3fbaf85343b4f280630aae89b17e3ae7f990dc2b
Tomohito Nakayama <tmht@7k8m.me>
2025-09-28 15:52:48 +0900

    Added logic to delete existingPdf

--- downloadPdf.sh
+++ downloadPdf.sh
@@ -1,6 +1,10 @@
 wget --recursive --accept .pdf https://www.entomophagy.or.jp/newsletter
+
+rm ./*.pdf
 mv www.entomophagy.or.jp/_files/ugd/*.pdf ./
+
 rm -rf www.entomophagy.or.jp
+
 git add .
 git commit -m "Update PDFs"
 git push origin main
